49d749a9154b03301dbf9179f344de1376b778f8
pshenmic <pshenmic@gmail.com>
2022-03-01 14:02:51 +0700

    fix: webpack.config.js is not needed

--- packages/platform-test-suite/karma.conf.js
+++ packages/platform-test-suite/karma.conf.js
@@ -9,8 +9,6 @@ const karmaChromeLauncher = require('karma-chrome-launcher');
 const karmaSourcemapLoader = require('karma-sourcemap-loader');
 const karmaWebpack = require('karma-webpack');
 
-const webpackConfig = require('./webpack.config');
-
 if (dotenvResult.error) {
   throw dotenvResult.error;
 }
@@ -20,30 +18,50 @@ module.exports = (config) => {
     frameworks: ['mocha', 'chai', 'webpack'],
     files: [
       'lib/test/karma/loader.js',
-      './**/*.spec.js',
-],
+      './test/functional/platform/featureFlags.spec.js',
+    ],
     preprocessors: {
       'lib/test/karma/loader.js': ['webpack', 'sourcemap'],
-      './**/*.spec.js': ['webpack', 'sourcemap'],
+      './test/functional/platform/featureFlags.spec.js': ['webpack', 'sourcemap']
     },
     webpack: {
       mode: 'development',
       devtool: 'inline-source-map',
       plugins: [
-        ...webpackConfig.plugins,
+        new webpack.ProvidePlugin({
+          Buffer: [require.resolve('buffer/'), 'Buffer'],
+          process: require.resolve('process/browser'),
+        }),
         new webpack.EnvironmentPlugin(
           dotenvResult.parsed,
         ),
       ],
-      resolve: webpackConfig.resolve,
+      resolve: {
+        fallback: {
+          fs: false,
+          crypto: require.resolve('crypto-browserify'),
+          buffer: require.resolve('buffer/'),
+          assert: require.resolve('assert/'),
+          url: require.resolve('url/'),
+          path: require.resolve('path-browserify'),
+          http: require.resolve('stream-http'),
+          https: require.resolve('https-browserify'),
+          stream: require.resolve('stream-browserify'),
+          util: require.resolve('util/'),
+          os: require.resolve('os-browserify/browser'),
+          zlib: require.resolve('browserify-zlib'),
+          events: require.resolve('events/'),
+          string_decoder: require.resolve('string_decoder/'),
+        },
+        extensions: ['.ts', '.js', '.json'],
+      },
     },
     reporters: ['mocha'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['ChromeHeadless'],
-    singleRun: false,
+    browsers: ['Chrome'],
+    singleRun: true,
     concurrency: Infinity,
     browserNoActivityTimeout: 10 * 60 * 1000,
     plugins: [
